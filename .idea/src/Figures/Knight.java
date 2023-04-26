1afc08af30ceabea091463324495885cbf9d70c4
Ludwig <ludwig.roeber@sqlxpert.de>
2023-04-26 13:45:54 +0200

    forgot to add

--- .idea/src/Figures/Knight.java
+++ .idea/src/Figures/Knight.java
@@ -1,2 +1,10 @@
-package Figures;public class Knight {
+package Figures;
+public class Knight extends Figure{
+
+    public Knight(boolean isblack, int position) {
+        this.value = 700;
+        this.isBlack = isblack;
+        this.position = position;
+        this.possibleMoves = (6, 10, 15, 17, 19, -6, -10, -15, -17 -19)
+    }
 }
